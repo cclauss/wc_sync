250bd8145fa32d8a0d21f2d6d9a6c7c18d0efc7a
Andrew <ahenry91@gmail.com>
2015-12-17 15:22:56 -0600

    Rewrite using class

--- Working_Copy_Sync.py
+++ Working_Copy_Sync.py
@@ -7,113 +7,142 @@ import urllib
 import base64
 import keychain
 
-#This should not have leading or trailing /
-INSTALL_PATH = 'wc_sync'
-
-#Key set in checkKey funtion
-key = None
-
-def close_view(sender):
-	sender.superview.close()
-
-def info():
-	documentsDir = os.path.expanduser('~/Documents')
-	info = editor.get_path()
-	fullPath = info[len(documentsDir)+1:] # get the relative path and remove the leading /
-	path = fullPath.split('/',1)[1]
-	repo = fullPath.split('/',1)[0]
-	return repo,path
-	
-def sendB64(repo,path,text):
-	url = 'working-copy://x-callback-url/write/?'
-	b64 = base64.b64encode(text)
-	f = {'repo':repo,'path':path,'key':key,'base64':b64,'x-success':'pythonista://'+repo+'/'+path+'?'}
-	url += urllib.urlencode(f).replace('+','%20')
-	wb.open(url)
-
-def sendText(repo,path,text):
-	url = 'working-copy://x-callback-url/write/?'
-	f = {'repo':repo,'path':path,'key':key,'text':text,'x-success':'pythonista://'+repo+'/'+path+'?'} # add repo and path to pythonista call back in order to not receive an error
-	url += urllib.urlencode(f).replace('+','%20')
-	wb.open(url)
-	
-def open_wc(sender): # Opens working copy
-	repo, path = info()
-	url = 'working-copy://open?'
-	f = {'repo':repo}
-	url += urllib.urlencode(f).replace('+','%20')
-	wb.open(url)
-	sender.superview.close()
-
-@ui.in_background
-def copyFromWCPt1(sender):
-	''' Copies the text from the working copy version 
-			of the file and uses it to overwrite the contents 
-			of the corresponding file in pythonista.
+def is_ipad():
+	''' Helper method to determine if we are on an 
+			iPad or an iPhone
 	'''
-	repo,path = info()
-	url = 'working-copy://x-callback-url/read/?'
-	success = 'pythonista://'+INSTALL_PATH+'/rxFile.py?action=run&argv=' + os.path.join(repo,path) +'&argv='
-	f = {'repo':repo,'path':path,'key':key, 'base64':'1'}
-	url += urllib.urlencode(f).replace('+','%20')
-	url += '&x-success=' + urllib.quote_plus(success)
-	wb.open(url)
-	sender.superview.close()
-
-def sendToWCPt1(sender):
-	''' Sends the contents of the file in pythonista 
-			to overwrite the working copy version.
-	'''
-	repo,path = info()
-	sendText(repo,path,editor.get_text())
-	sender.superview.close()
-
-def sendPYUIToWCPt1(sender):
-	''' Send pyui to working copy 
-	'''
-	repo,path = info()
-	path += 'ui'
-	fullPath = os.path.join(os.path.expanduser('~/Documents'), os.path.join(repo,path))
-	try:
-		with open(fullPath) as file:
-			sendB64(repo,path,file.read())
-	except IOError:
-		console.alert('No PYUI file associated. Now say you\'re sorry.' ,button1='I\'m sorry.', hide_cancel_button=True)
-	sender.superview.close()
-		
-def getZipPt1(sender):
-	''' copy a repo to the local filesystem 
-	'''
-	repo = console.input_alert('Repo name')
-	if repo: 
-		url = 'working-copy://x-callback-url/zip/?'
-		f = {'repo':repo, 'key':key}
-		success ='pythonista://'+INSTALL_PATH+'/rxZip.py?action=run&argv='+repo+'&argv='
-		url += urllib.urlencode(f).replace('+','%20')
-		url += '&x-success=' + urllib.quote_plus(success)
-		wb.open(url)
-	else:
-		console.alert('Invalid repo name')
-	sender.superview.close()
-
-def checkKey():
-	global key
-	key = keychain.get_password('wcSync','xcallback')
-	if key == None:
-		pwd = console.password_alert('Working Copy Key')
-		keychain.set_password('wcSync','xcallback',pwd)
-	
-def main():
-	checkKey()
-	view = ui.load_view('Working_Copy_Sync')
 	width, height = ui.get_screen_size()
-	try:
-		if width >= 768:
-			view.present('sheet', hide_title_bar=True)
+	if width >= 768:
+		return True
+	else:
+		return False
+		
+def close_view(sender):
+	''' Helper method to put a close (X) button in 
+			the pyui view
+	'''
+	sender.superview.close()
+
+class WorkingCopySync():
+	
+	def __init__(self):
+		self.key = self._get_key()
+		self.install_path = self._find_install_path()
+		self.view = ui.load_view('Working_Copy_Sync')
+		self.repo, self.path = self._get_repo_info()
+		
+	def _get_key(self):
+		''' Retrieve the working copy key or prompt for a new one. 
+		'''
+		key = keychain.get_password('wcSync', 'xcallback')
+		if not key:
+			key = console.password_alert('Working Copy Key')
+			keychain.set_password('wcSync', 'xcallback', key)
+		return key
+	
+	def _find_install_path(self):
+		''' Dynamically find the installation path for the script
+		'''
+		app_dir = os.path.realpath(os.path.abspath(os.path.dirname(__file__)))
+		home_dir = os.path.join(os.environ['HOME'], 'Documents')
+		return os.path.relpath(app_dir, home_dir)
+	
+	def _get_repo_info(self):
+		documentsDir = os.path.expanduser('~/Documents')
+		info = editor.get_path()
+		fullPath = info[len(documentsDir)+1:] # get the relative path and remove the leading /
+		path = fullPath.split('/',1)[1]
+		repo = fullPath.split('/',1)[0]
+		return repo, path			
+		
+	def _send_to_working_copy(self, action, payload, x_callback_enabled=True):
+		payload['key'] = self.key
+		if x_callback_enabled:
+			url = 'working-copy://x-callback-url/{action}/?{payload}'
 		else:
-			view.present(hide_title_bar=True)
-	except KeyboardInterrupt:
-		pass
+			url = 'working-copy://{action}/?{payload}'
+		urlencoded_payload = urllib.urlencode(payload).replace('+', '%20')
+		url = url.format(action=action, payload=urlencoded_payload)
+		wb.open(url)
+
+	def copy_repo_from_wc(self, sender):
+		''' copy a repo to the local filesystem 
+		'''
+		repo_name = console.input_alert('Repo name')
+		if not repo_name:
+			console.alert('Invalid repo name')
+		else:
+			action = 'zip'
+			payload = {
+				'repo': repo_name,
+				'x-success': 'pythonista://{install_path}/rxZip.py?action=run&argv={repo_name}&argv='.format(install_path=self.install_path, repo_name=repo_name)
+			}
+			self._send_to_working_copy(action, payload)
+		sender.superview.close()		
+		
+	def _push_file_to_wc(self, path, contents):
+		action = 'write'
+		payload = {
+			'repo': self.repo,
+			'path': path,
+			'text': contents,
+			'x-success': 'pythonista://{repo}/{path}?'.format(repo=self.repo, path=path)
+		}
+		self._send_to_working_copy(action, payload)
+			
+	def push_current_file_to_wc(self, sender):
+		self._push_file_to_wc(self.path, editor.get_text())
+		sender.superview.close()
+		
+	def push_pyui_to_wc(self, sender):
+		pyui_path, pyui_contents = self._get_pyui_contents_for_file()
+		if not pyui_contents:
+			console.alert('No PYUI file associated. Now say you\'re sorry.' ,button1='I\'m sorry.', hide_cancel_button=True)
+		else:
+			self._push_file_to_wc(pyui_path, pyui_contents)
+			sender.superview.close()
+		
+	def _get_pyui_contents_for_file(self):
+		rel_pyui_path = self.path + 'ui'
+		full_pyui_path = os.path.join(os.path.expanduser('~/Documents'), os.path.join(self.repo, rel_pyui_path))
+		try:
+			with open(full_pyui_path) as f:
+				return rel_pyui_path, f.read()
+		except IOError:
+			return None, None
+			
+	def overwrite_with_wc_copy(self, sender):
+		action = 'read'
+		payload = {
+			'repo': self.repo,
+			'path': self.path,
+			'base64': '1',
+			'x-success': 'pythonista://{install_path}/rxFile.py?action=run&argv={full_path}&argv='.format(install_path=self.install_path, full_path=os.path.join(self.repo, self.path))
+		}
+		self._send_to_working_copy(action, payload)
+		sender.superview.close()		
+		
+	def open_repo_in_wc(self, sender):
+		action = 'open'
+		payload = {
+			'repo': self.repo
+		}
+		self._send_to_working_copy(action, payload)
+		sender.superview.close()
+
+	def present(self):
+		try:
+			if is_ipad():
+				self.view.present('sheet', hide_title_bar=True)
+			else:
+				self.view.present(hide_title_bar=True)
+		except KeyboardInterrupt:
+			pass		
+			
+
+def main():
+	wc = WorkingCopySync()
+	wc.present()
 
 if __name__ == "__main__":
 	main()
